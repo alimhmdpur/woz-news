8ba3f027890a2e9585af3269464b8bd0a431ddf6
ali mhmdpur <alimhmdpur999@gmail.com>
2023-07-25 19:45:00 +0330

    sc

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,27 +1,53 @@
-const swiper = new Swiper(".imgTeam", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+const swiper = new Swiper(".discTeam", {
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    next: {
+      translate: ["100%", 0, 0],
     },
-  });
+  },
+});
 
-const swiper2 = new Swiper(".discTeam", {
-    grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-      prev: {
-        shadow: true,
-        translate: [0, 0, -400],
-      },
-      next: {
-        translate: ["100%", 0, 0],
-      },
-    },
-  });
+const swiper2 = new Swiper(".imgTeam", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+//  Swipe to next button when  clicking        on next       button on       swiper        button   on swiper button       on swiper button on
+
+const swipe3 = new Swiper(".discSoldier", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+});
+
+const swiper4 = new Swiper(".imgSoldier", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swipe3,
+  },
+});
