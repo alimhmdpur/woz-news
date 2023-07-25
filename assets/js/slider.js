91336bd2571255b57827b961ea5ceb140f23d335
ali mhmdpur <alimhmdpur999@gmail.com>
2023-07-25 15:37:36 +0330

    da

---
+++ assets/js/slider.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper(".imgTeam", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+const swiper2 = new Swiper(".discTeam", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: [0, 0, -400],
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
+  });
